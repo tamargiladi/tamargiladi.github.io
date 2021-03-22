f0352d11b77a8aa922113c3203c0e9d92162ceeb
tmrgldy <tmrgldy@gmail.com>
2021-03-22 13:51:09 +0200

    22.03.2021 - #2

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,3 +1,11 @@
+function hideAll_Marks()
+{
+    document.getElementById("p_about_me").style.backgroundColor="var(--background)";
+    document.getElementById("p_contact").style.backgroundColor="var(--background)";
+    document.getElementById("p_skills").style.backgroundColor="var(--background)";
+
+
+}
 function hideAll_onload()
 {
     document.getElementById("about_me_box").style.display="none";
@@ -14,12 +22,22 @@ function hideAll() {
 function openDiv(div)
 {
     hideAll();
+    hideAll_Marks();
 
     if(div==1)//about me
-        document.getElementById("about_me_box").style.display="flex";
+    {
+        document.getElementById("about_me_box").style.display = "flex";
+        document.getElementById("p_about_me").style.backgroundColor="var(--third)";
+    }
     else if(div==2)//skills
-        document.getElementById("main_skills_box").style.display="block";
+    {        document.getElementById("main_skills_box").style.display="block";
+            document.getElementById("p_skills").style.backgroundColor="var(--third)";
+
+    }
     else
+    {
         document.getElementById("contact_box").style.display="flex";
+        document.getElementById("p_contact").style.backgroundColor="var(--third)";
+    }
 
 }
