37dbfbec0cfe1b8f56cb7667d97fcf89e851bafe
tmrgldy <tmrgldy@gmail.com>
2021-03-26 08:05:58 +0300

    26.03 #Commit with project section

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -3,6 +3,8 @@ function hideAll_Marks()
     document.getElementById("p_about_me").style.backgroundColor="var(--background)";
     document.getElementById("p_contact").style.backgroundColor="var(--background)";
     document.getElementById("p_skills").style.backgroundColor="var(--background)";
+    document.getElementById("p_projects").style.backgroundColor="var(--background)";
+
 
 
 }
@@ -10,12 +12,16 @@ function hideAll_onload()
 {
     document.getElementById("about_me_box").style.display="none";
     document.getElementById("contact_box").style.display="none";
+    document.getElementById("projects_box").style.display="none";
+
 }
 
 function hideAll() {
     document.getElementById("main_skills_box").style.display="none";
     document.getElementById("about_me_box").style.display="none";
     document.getElementById("contact_box").style.display="none";
+    document.getElementById("projects_box").style.display="none";
+
 
 }
 
@@ -34,10 +40,15 @@ function openDiv(div)
             document.getElementById("p_skills").style.backgroundColor="var(--third)";
 
     }
-    else
+    else if(div==3)
     {
         document.getElementById("contact_box").style.display="flex";
         document.getElementById("p_contact").style.backgroundColor="var(--third)";
+    }
+    else if(div==4)
+    {
+        document.getElementById("projects_box").style.display="flex";
+        document.getElementById("p_projects").style.backgroundColor="var(--third)";
     }
 
 }
